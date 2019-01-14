dcf943edf6a90a5c21f24e46a49338f18f3a622b
Guilhem Saurel <guilhem.saurel@gmail.com>
2019-01-14 14:11:52 +0100

    Time

--- src/signal.cpp
+++ src/signal.cpp
@@ -4,6 +4,7 @@
 
 namespace dynamicgraph
 {
+  typedef int Time;
 
   void translate(ExceptionSignal const& e)
   {
@@ -16,33 +17,33 @@ namespace dynamicgraph
 
     register_exception_translator<ExceptionSignal>(&translate);
 
-    class_<SignalBase<int>, boost::noncopyable>("SignalBase", init<std::string>())
-      .add_property("time", make_function(&SignalBase<int>::getTime, return_value_policy<copy_const_reference>()), &SignalBase<int>::setTime)
-      .add_property("ready", make_function(&SignalBase<int>::getReady, return_value_policy<copy_const_reference>()))
-      .add_property("name", make_function(&SignalBase<int>::getName, return_value_policy<copy_const_reference>()))
-      //.add_property("class_name", &SignalBase<int>::getClassName)
-      .add_property("period_time", &SignalBase<int>::getPeriodTime, &SignalBase<int>::setPeriodTime)
-      .def("add_dependency", &SignalBase<int>::addDependency)
-      .def("remove_dependency", &SignalBase<int>::removeDependency)
-      .def("clear_dependencies", &SignalBase<int>::clearDependencies)
-      .def("need_update", &SignalBase<int>::needUpdate)
-      .def("set_ready", &SignalBase<int>::setReady)
-      .def("plug", &SignalBase<int>::plug)
-      .def("is_plugged", &SignalBase<int>::isPlugged)
-      //.def("get_plugged", &SignalBase<int>::getPluged)
-      .def("set_constant_default", &SignalBase<int>::setConstantDefault)
-      .def("set", &SignalBase<int>::set)
-      .def("get", &SignalBase<int>::get)
-      .def("recompute", &SignalBase<int>::recompute)
-      .def("trace", &SignalBase<int>::trace)
-      .def("short_name", &SignalBase<int>::shortName)
-      //.def("extract_node_and_local_names", &SignalBase<int>::extractNodeAndLocalNames)
-      .def("check_compatibility", &SignalBase<int>::checkCompatibility)
+    class_<SignalBase<Time>, boost::noncopyable>("SignalBase", init<std::string>())
+      .add_property("time", make_function(&SignalBase<Time>::getTime, return_value_policy<copy_const_reference>()), &SignalBase<Time>::setTime)
+      .add_property("ready", make_function(&SignalBase<Time>::getReady, return_value_policy<copy_const_reference>()))
+      .add_property("name", make_function(&SignalBase<Time>::getName, return_value_policy<copy_const_reference>()))
+      //.add_property("class_name", &SignalBase<Time>::getClassName)
+      .add_property("period_time", &SignalBase<Time>::getPeriodTime, &SignalBase<Time>::setPeriodTime)
+      .def("add_dependency", &SignalBase<Time>::addDependency)
+      .def("remove_dependency", &SignalBase<Time>::removeDependency)
+      .def("clear_dependencies", &SignalBase<Time>::clearDependencies)
+      .def("need_update", &SignalBase<Time>::needUpdate)
+      .def("set_ready", &SignalBase<Time>::setReady)
+      .def("plug", &SignalBase<Time>::plug)
+      .def("is_plugged", &SignalBase<Time>::isPlugged)
+      //.def("get_plugged", &SignalBase<Time>::getPluged)
+      .def("set_constant_default", &SignalBase<Time>::setConstantDefault)
+      .def("set", &SignalBase<Time>::set)
+      .def("get", &SignalBase<Time>::get)
+      .def("recompute", &SignalBase<Time>::recompute)
+      .def("trace", &SignalBase<Time>::trace)
+      .def("short_name", &SignalBase<Time>::shortName)
+      //.def("extract_node_and_local_names", &SignalBase<Time>::extractNodeAndLocalNames)
+      .def("check_compatibility", &SignalBase<Time>::checkCompatibility)
       ;
 
   }
   void expose_signal()
   {
-    //class_<Signal<Vector, int>>("Signal", init<std::string>());
+    //class_<Signal<Vector, Time>>("Signal", init<std::string>());
   }
 }
