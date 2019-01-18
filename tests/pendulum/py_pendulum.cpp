dd5a51bf48b35a80657b9da85f6f242877ea64b4
Guilhem Saurel <guilhem.saurel@laas.fr>
2019-01-18 17:09:48 +0100

    fix details for ubuntu

--- tests/pendulum/py_pendulum.cpp
+++ tests/pendulum/py_pendulum.cpp
@@ -10,7 +10,7 @@ BOOST_PYTHON_MODULE(libpy_pendulum)
 
   expose_entity();
 
-  class_<InvertedPendulum, bases<Entity>, boost::noncopyable>("InvertedPendulum", init<std::string, optional<double, double, double, double>>())
+  class_<InvertedPendulum, bases<Entity>, boost::noncopyable>("InvertedPendulum", init<std::string, optional<double, double, double, double> >())
     .add_property("cart_mass", &InvertedPendulum::getCartMass, &InvertedPendulum::setCartMass)
     .add_property("pendulum_mass", &InvertedPendulum::getPendulumMass, &InvertedPendulum::setPendulumMass)
     .add_property("pendulum_length", &InvertedPendulum::getPendulumLength, &InvertedPendulum::setPendulumLength)
--- tests/pendulum/py_pendulum.py
+++ tests/pendulum/py_pendulum.py
@@ -1,8 +1,9 @@
 import unittest
+from collections import OrderedDict
 
 from libpy_pendulum import InvertedPendulum
 
-MEMBERS = {'cart_mass': 4.2, 'pendulum_mass': 2.3, 'pendulum_length': 0.8, 'viscosity': 0.5}
+MEMBERS = OrderedDict([('cart_mass', 4.2), ('pendulum_mass', 2.3), ('pendulum_length', 0.8), ('viscosity', 0.5)])
 
 
 class TestInvertedPendulum(unittest.TestCase):
